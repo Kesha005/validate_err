c8824ba1770500560f8edcbc0278acc8a961fa9c
Kesha005 <keshasaparow@mail.ru>
2023-10-19 18:04:18 +0500

    Contact_Handler

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -10,4 +10,23 @@ class Contact extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'email', 'job'];
+
+    public function __construct($data)
+    {
+        $this->name = $this->name;
+        $this->email = $this->email;
+        $this->job = $this->job;
+    }
+
+    public  function store(){
+        $this->create([
+            'name'=>$this->name,
+            'email'=>$this->email,
+            'job'=>$this->job
+        ]);
+    }
+
+    public static function update_contact($id, $data){
+        $contact = self::where('id', $id)->first();
+    }
 }
