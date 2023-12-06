eebd8a3b9f2b994b8ac4ee9ff0dcb7596fc64d37
Kerimberdi Saparow <keshasaparow@mail.ru>
2023-12-06 23:09:34 +0500

    AddedSomeBasicValidationMessages

--- error_msg.go
+++ error_msg.go
@@ -14,13 +14,35 @@ type ErrorMessage struct{
 	Message string `"json:message"`
 }
 
+var valerr validator.ValidationErrors
+
+func GetErr(err error){
+	if errors.As(err, &valerr){
+
+	}
+}
+
 func GetErrorMsg(fe validator.FieldError)string{
 
 	switch fe.Tag(){
+
 		case "required":
-			return ""
+			return "This field is required"
+
 		case "min":
-			return ""
+			return " The min value must be "+ fe.Param()
+		
+		case "max":
+			return "The maximum value must be "+ fe.Param()
+		case "let":
+			return "Should be less than "+ fe.Param()
+
+		case "startwith":
+			return "Value must be start with "+fe.Param()
+
+		case "endswith":
+			return "Value must be end with "+fe.Param()
 	}
 	return "Unknown error"
 }	
+
