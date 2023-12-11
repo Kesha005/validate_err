ad19767b8717f08793970ffca62d1797d6f73613
Kerimberdi Saparow <keshasaparow@mail.ru>
2023-12-11 13:03:45 +0500

    modifing

--- getString.go
+++ getString.go
@@ -14,7 +14,7 @@ func GetField(rules string) string {
 
 	return "Unknown"
 }
-
+//Code will be modifed there
 func GetRule(rules string) string {
 	for t := 0; t < len(rules); t++ {
 		if string(rules[t]) == "." {
